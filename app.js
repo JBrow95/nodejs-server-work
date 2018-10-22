7343f5e968a1b666c582abbb2f449ade4db68e0f
Jbrown95 <browningjustin95@gmail.com>
2018-10-22 18:59:04 -0400

    added middleware for body parser

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var path = require('path');
 
 var app = express();
+
+// Body Parser Middleware
+app.use(bodyParser.jason());
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.get('/', function(req, res){
   res.send("Hello World");
 });
 
 app.listen(3000, function(){
-  console.log("Yo server just started boss");
+  console.log('Server is currently running..);
 })
