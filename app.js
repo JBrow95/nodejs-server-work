4b4e76e5f5d3c7c536850c542f820749697d7fae
Jbrown95 <browningjustin95@gmail.com>
2018-10-22 18:49:33 -0400

    created get request and printed hello world to the page

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ var path = require('path');
 
 var app = express();
 
+app.get('/', function(req, res){
+  res.send("Hello World");
+});
+
 app.listen(3000, function(){
   console.log("Yo server just started boss");
 })
